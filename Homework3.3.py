1a2ccb81e8a08971d5e9269a4a9ecbb3fa2027d3
Admbox <mailv.novikov@gmail.com>
2025-04-02 18:04:21 +0200

    Homework 3.3

---
+++ Homework3.3.py
@@ -0,0 +1,10 @@
+def split_list(lst):
+    if len(lst) == 0:
+        return [[], []]
+
+    mid = (len(lst) + 1) // 2
+    return [lst[:mid], lst[mid:]]
+
+print(split_list([1, 2, 3, 4, 5]))
+print(split_list([1, 2, 3, 4]))
+print(split_list([]))
