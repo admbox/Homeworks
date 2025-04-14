c15d8c72a7e06adea1c73cce786797974318251c
admbox <mailv.novikov@gmail.com>
2025-04-14 16:52:11 +0200

    Update Homework6.2.py
    fix

--- Homework6.2.py
+++ Homework6.2.py
@@ -9,11 +9,11 @@ hours, remaining_seconds = divmod(remaining_seconds, SECONDS_IN_HOUR)
 minutes, seconds = divmod(remaining_seconds, SECONDS_IN_MINUTE)
 
 if days == 1:
-    day_word = "day"
+    day_word = "день"
 elif 2 <= days % 10 <= 4 and not 12 <= days % 100 <= 14:
-    day_word = "days"
+    day_word = "дни"
 else:
-    day_word = "dayss"
+    day_word = "дней"
 
 time_str = f"{str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}"
 
